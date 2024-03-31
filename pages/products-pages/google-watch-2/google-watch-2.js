5c60666855b261631d3d0a43358c7d072c011122
unknown <nursultanr585@gmail.com>
2024-03-31 21:28:02 +0500

    google watch 2 page code

--- pages/products-pages/google-watch-2/google-watch-2.js
+++ pages/products-pages/google-watch-2/google-watch-2.js
@@ -2,6 +2,13 @@ const heroIntro = document.querySelector('.hero__intro')
 const heroVideo = document.querySelector('.hero__video')
 const heroInner = document.querySelector('.hero__inner')
 
+const checkButtonSigns = document.querySelector('.check__button-signs')
+const checkButtonsSteps = document.querySelector('.check__button-steps')
+const checkButtonsRoutine = document.querySelector('.check__button-routine')
+
+const checkSignsVideo = document.querySelector('.check__signs-video')
+const checkStepsVideo = document.querySelector('.check__steps-video')
+const checkRoutineVideo = document.querySelector('.check__routine-image')
 
 // https://storage.googleapis.com/mannequin/blobs/4d177599-0950-4f70-866d-4c4f92b74632.mp4
 heroIntro.addEventListener("ended",() => {
@@ -11,3 +18,33 @@ heroIntro.addEventListener("ended",() => {
     heroInner.classList.remove("hidden");
 
 }, {once:true})
+
+checkButtonSigns.addEventListener(('click') , () => {
+    checkSignsVideo.classList.remove('hidden')
+    checkStepsVideo.classList.add('hidden')
+    checkRoutineVideo.classList.add('hidden')
+
+    checkButtonSigns.setAttribute('disabled',0)
+    checkButtonsSteps.removeAttribute('disabled')
+    checkButtonsRoutine.removeAttribute('disabled')
+})
+
+checkButtonsSteps.addEventListener(('click') , () => {
+    checkSignsVideo.classList.add('hidden')
+    checkStepsVideo.classList.remove('hidden')
+    checkRoutineVideo.classList.add('hidden')
+
+    checkButtonSigns.removeAttribute('disabled')
+    checkButtonsSteps.setAttribute('disabled',0)
+    checkButtonsRoutine.removeAttribute('disabled')
+})
+
+checkButtonsRoutine.addEventListener(('click') , () => {
+    checkSignsVideo.classList.add('hidden')
+    checkStepsVideo.classList.add('hidden')
+    checkRoutineVideo.classList.remove('hidden')
+
+    checkButtonSigns.removeAttribute('disabled')
+    checkButtonsSteps.removeAttribute('disabled')
+    checkButtonsRoutine.setAttribute('disabled',0)
+})
--- server/google-api/obj/Debug/net8.0/google-api.AssemblyInfo.cs
+++ server/google-api/obj/Debug/net8.0/google-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("google-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfa532918aa0a348905a94113e66f5a91c5eea6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b1cedb4248e06ef28dd2820fff05836075a8478")]
 [assembly: System.Reflection.AssemblyProductAttribute("google-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("google-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
